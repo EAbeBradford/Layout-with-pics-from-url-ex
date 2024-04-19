90a4115208af124c9b2b87c324576af2eea96f99
Mx. Bradford <emma_bradford@milton.com>
2024-04-19 15:31:32 -0400

    made basic photo from url

---
+++ src/SwingControlDemo.java
@@ -0,0 +1,172 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class SwingControlDemo implements ActionListener {
+    private JFrame mainFrame;
+    private JLabel statusLabel;
+    private JPanel imagePanel;
+    private JMenuBar mb;
+    private JMenu file, edit, help;
+    private JMenuItem cut, copy, paste, selectAll;
+    private JTextArea ta; //typing area
+    private int WIDTH = 800;
+    private int HEIGHT = 700;
+
+
+    public SwingControlDemo() {
+        prepareGUI();
+    }
+
+    public static void main(String[] args) throws IOException {
+        SwingControlDemo swingControlDemo = new SwingControlDemo();
+        swingControlDemo.addImage();
+    }
+
+    private void prepareGUI() {
+        mainFrame = new JFrame("Layout Images are awesome");
+        mainFrame.setSize(WIDTH, HEIGHT);
+        mainFrame.setLayout(new BorderLayout());
+
+        //menu at top
+        cut = new JMenuItem("cut");
+        copy = new JMenuItem("copy");
+        paste = new JMenuItem("paste");
+        selectAll = new JMenuItem("selectAll");
+        cut.addActionListener(this);
+        copy.addActionListener(this);
+        paste.addActionListener(this);
+        selectAll.addActionListener(this);
+
+        mb = new JMenuBar();
+        file = new JMenu("File");
+        edit = new JMenu("Edit");
+        help = new JMenu("Help");
+        edit.add(cut);
+        edit.add(copy);
+        edit.add(paste);
+        edit.add(selectAll);
+        mb.add(file);
+        mb.add(edit);
+        mb.add(help);
+        //end menu at top
+
+        ta = new JTextArea("url: ");
+        ta.setBounds(50, 5, WIDTH - 100, HEIGHT - 50);
+        mainFrame.add(mb);  //add menu bar
+        mainFrame.add(ta, BorderLayout.NORTH);//add typing area
+        mainFrame.setJMenuBar(mb); //set menu bar
+
+        //  statusLabel = new JLabel("Label", JLabel.CENTER);
+        //   statusLabel.setSize(350, 100);
+
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+        imagePanel = new JPanel();
+        //controlPanel.setLayout(); //set the layout of the pannel
+
+        //  mainFrame.add(controlPanel);
+        // mainFrame.add(statusLabel);
+        mainFrame.setVisible(true);
+    }
+
+    private void addImage() throws IOException {
+        try {
+            String path = "";
+            if (!ta.getText().contains("http")) {
+                path = "https://i.pinimg.com/originals/07/16/ba/0716ba54fe3b77b3a5b0b16c7bc33389.png";
+            } else {
+                path = ta.getText();
+                if (path.contains("url")) {
+                    path = path.substring(path.indexOf("http"));
+                }
+            }
+
+
+            URL url = new URL(path);
+
+            BufferedImage weiYing = ImageIO.read(url.openStream());
+           // weiYing =
+
+            ImageIcon WUXIAN;
+            if (weiYing != null) {
+                WUXIAN = new ImageIcon(weiYing.getScaledInstance(800, 700, Image.SCALE_SMOOTH));
+                JLabel imageLabel;// = new JLabel();
+                if (WUXIAN != null) {
+                    imageLabel = new JLabel(WUXIAN);
+                } else {
+                    imageLabel = new JLabel("did not load");
+                }
+                imagePanel.removeAll();
+                imagePanel.add(imageLabel);
+                mainFrame.add(imagePanel, BorderLayout.CENTER);
+
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+            System.out.println("sadness");
+
+            // Image weiYing = ImageIO.read(url);
+            //System.out.println("hi");
+            BufferedImage myPicture = ImageIO.read(new File("WeiYing.png"));
+            JLabel imageLabel = new JLabel(new ImageIcon(myPicture.getScaledInstance(800, 700, Image.SCALE_SMOOTH)));
+
+            imagePanel.removeAll();
+            imagePanel.add(imageLabel);
+            mainFrame.add(imagePanel);
+
+        }
+
+//        JButton okButton = new JButton("OK!!!!!!!!!");
+//        JButton submitButton = new JButton("Submit");
+//        JButton cancelButton = new JButton("Cancel");
+//
+//        okButton.setActionCommand("OK");
+//        submitButton.setActionCommand("Submit");
+//        cancelButton.setActionCommand("Cancel");
+//
+//        okButton.addActionListener(new ButtonClickListener());
+//        submitButton.addActionListener(new ButtonClickListener());
+//        cancelButton.addActionListener(new ButtonClickListener());
+//
+//        controlPanel.add(okButton, BorderLayout.EAST);
+//        controlPanel.add(submitButton, BorderLayout.CENTER);
+//        controlPanel.add(cancelButton, BorderLayout.WEST);
+
+        mainFrame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == cut)
+            ta.cut();
+        if (e.getSource() == paste)
+            ta.paste();
+        if (e.getSource() == copy)
+            ta.copy();
+        if (e.getSource() == selectAll)
+            ta.selectAll();
+    }
+
+    private class ButtonClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+
+            if (command.equals("OK")) {
+                statusLabel.setText("Ok Button clicked.");
+            } else if (command.equals("Submit")) {
+                statusLabel.setText("Submit Button clicked.");
+            } else {
+                statusLabel.setText("Cancel Button clicked.");
+            }
+        }
+    }
+}
